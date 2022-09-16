50d7386dd2dda186fa5a8b1bc9bb1d2f900301fb
Your Name <you@example.com>
2022-09-16 17:36:43 +0530

    commiting java files

---
+++ Addition.java
@@ -0,0 +1,26 @@
+class Addition{
+
+    public static void main(String c[]){ 
+	
+	// calling method
+	 add (89,78);
+	 add (78,67);
+	 add (456,67); 
+	 add (123,123);
+	 add  (98,100);
+     
+	 }
+		
+	 //add is a method
+	  public static void add(int a, int b){
+        System.out.println(a+b);
+         
+		 
+		 }		
+	
+	
+	
+	
+	
+	
+	  }
