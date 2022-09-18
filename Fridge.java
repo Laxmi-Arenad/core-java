37d6b8036e67b392f22ee0515ee3aa066831fee4
Your Name <you@example.com>
2022-09-18 22:17:40 +0530

    committing java files

---
+++ Fridge.java
@@ -0,0 +1,22 @@
+  class Fridge{ 
+
+     static String brandName;
+     static String color;
+     static int price;
+ 
+     public static void main(String a[]){ 
+   
+      Fridge.brandName = "Gojrej";
+	  Fridge.color =  "Chocolate";
+	  Fridge.price = 30000;
+	  
+	   System.out.println(Fridge.brandName);
+	   System.out.println(Fridge.color);
+	   System.out.println(Fridge.price);
+	   
+	  
+ 
+ }
+ 
+ 
+ }
