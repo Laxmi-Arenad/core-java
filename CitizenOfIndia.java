e467c572706505a834c3ef0797f7f426b1bca33c
Your Name <you@example.com>
2022-09-22 13:15:11 +0530

    commiting methods related files

---
+++ CitizenOfIndia.java
@@ -0,0 +1,36 @@
+class CitizenOfIndia{ 
+
+ public static void showTheResponsibility(){ 
+ System.out.println("Invocked showTheResponsibility");
+  
+  int age = 16;
+  char gender = 'F';
+  // AND OPERATOR
+  // OR OPERATOR
+  
+  if(age >=16 && age <16){
+	  
+	  System.out.println("Responsibility is school");
+	  return;
+  }
+  
+  if (age ==16){
+  
+	  System.out.println("Responsibility is college");
+	  return ;
+	  }
+	  
+	if (age ==18){
+	
+		System.out.println("Responsibility of voting");
+		return;
+	}
+	if (age ==21{
+	
+		System.out.println("Girl can legally marry");
+		return;
+  
+ 
+  }
+
+   }
