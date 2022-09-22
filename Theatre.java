4f9b6720a3ee100fe85c306bc37994af0e9becfe
Your Name <you@example.com>
2022-09-22 23:09:25 +0530

    committing array related files

---
+++ Theatre.java
@@ -0,0 +1,37 @@
+class Theatre {
+	
+	static long priceofkannadamovies[] = {1000,150,150,200,90000L};
+	static long priceofhindimovies []= {1000,150,150,200,90000L};
+	static String kannadaMovies[] = {"KGF Franchies","Om","Sampathige sawaal","Googly",}; static String hindiMovies [] = {"3 Idiots" ,"DDLJ","PK","bhajarangi bhaiijaan"};
+    
+ 
+ 
+ //static String movieName3
+ 
+ 
+ public static void main(String a []){
+ 
+  System.out.println(" List of  kannadaMovies ");
+  System.out.println(" kannadaMovies[0] " + " " + " kannadaMovies[1] "+ " " + " kannadaMovies [2] " + " " + " kannadaMovies[3] "+ " " );
+   
+   System.out.println(" List of hindiMovies ");
+   System.out.println(" hindiMovies[0] " + " " + " hindiMovies[1] " + " " +" hindiMovies[2] " + " " + " hindiMovies [3] " + " " );
+   
+   
+ 
+ 
+ }
+  
+   
+  
+  
+  
+  
+  }  
+
+
+
+
+
+
+
