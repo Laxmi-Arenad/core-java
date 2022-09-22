da9598bae1fac32800efa7e6e8500ed75e84bb16
Your Name <you@example.com>
2022-09-23 00:04:08 +0530

    committing methodoverloading related files

---
+++ Addition.java
@@ -0,0 +1,38 @@
+class Addition2{ 
+  
+  //add method declaration
+  
+ public static void add(int a , int b,){
+ {
+ 
+ System.out.println(a+b);
+ // System.out.println(a+b+k);
+ }
+ // method overloading
+ public static void add(int a, int b, int k)
+ { 
+ //System.out.println(a+b);
+ 
+ System.out.println(a+b+k);
+  }
+  }
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ AdditionTester.java
@@ -0,0 +1,13 @@
+class AdditionTester{ 
+
+ public static void main (String a[]){
+ 
+  Addition.add(23,24);
+  Addition.add(26,78,67);
+ }
+ 
+  }
+
+
+
+
