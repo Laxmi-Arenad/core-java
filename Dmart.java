2700271be6f4b6b3fdeb661b7e88e745fec310ac
Your Name <you@example.com>
2022-09-22 23:03:04 +0530

    committing method related files

---
+++ Dmart.java
@@ -0,0 +1,104 @@
+class Dmart{
+
+
+static void getVegatables(String[] vegatables){ 
+System.out.println("inside getVegatables()");
+System.out.println("Size of vegatables is" + vegatables.length);
+System.out.println(" List of vegatables are");
+for(int index =0; index < vegatables.length; index++){
+	System.out.println(vegatables[index]);
+	}
+	System.out.println("end of getVegatables ");
+}
+ static void getPerfumes(String[] perfumes){
+ System.out.println("inside getPerfumes()");
+ System.out.println("Size of perfumes is" + perfumes.length);
+ System.out.println("{List of perfumes are");
+ for(int index = 0 ; index < perfumes.length ; index++){ 
+ System.out.println(perfumes[index]);
+ }
+ System.out.println("end of getperfumes");
+ }
+
+  static void getBiscuites(String[] biscuites){
+  System.out.println("inside getBiscuites()");
+  System.out.println("Size of biscuites is" + biscuites.length);
+  System.out.println("List of biscuites are");
+  for(int index=0 ; index < biscuites.length; index++){
+  System.out.println(biscuites[index]);
+ }
+ System.out.println("end of getBiscuites");
+ }
+
+ 
+  static void getChocolates(String[] chocolates){
+  System.out.println("inside getChocolates()");
+  System.out.println("Size of chocolatesis" + chocolates.length);
+  System.out.println("List of chocolates are");
+  for(int index=0 ; index < chocolates.length; index++){
+  System.out.println(chocolates[index]);
+ }
+ System.out.println("end of getChocolates");
+ }
+
+ static void getMeat(String[] meat){
+  System.out.println("inside getMeat()");
+  System.out.println("Size of Meat is" + meat.length);
+  System.out.println("List of meat are");
+  for(int index=0 ; index < meat.length; index++){
+  System.out.println(meat[index]);
+ }
+ System.out.println("end of getMeat");
+ }
+
+ static void getElectronicDevices(String[] electronicDevices){
+  System.out.println("inside getElectronicDevices()");
+  System.out.println("Size of ElectronicDevices is" + electronicDevices.length);
+  System.out.println("List of electronicDevices are");
+  for(int index=0 ; index < electronicDevices.length; index++){
+  System.out.println(electronicDevices[index]);
+ }
+ System.out.println("end of getElectronicDevices");
+ }
+
+ static void getClothes(String[] clothes){
+  System.out.println("inside getClothes()");
+  System.out.println("Size of clothes is" + clothes.length);
+  System.out.println("List of clothes are");
+  for(int index=0 ; index < clothes.length; index++){
+  System.out.println(clothes[index]);
+ }
+ System.out.println("end of getClothes");
+ }
+
+ static void getShoes(String[] shoes){
+  System.out.println("inside getShoes()");
+  System.out.println("Size of shoesis" + shoes.length);
+  System.out.println("List of shoes are");
+  for(int index=0 ; index < shoes.length; index++){
+  System.out.println(shoes[index]);
+ }
+ System.out.println("end of getShoes");
+ }
+
+ static void getGroceries(String[] groceries){
+  System.out.println("inside getGroceries()");
+  System.out.println("Size of groceries is" + groceries.length);
+  System.out.println("List of groceries are");
+  for(int index=0 ; index < groceries.length; index++){
+  System.out.println(groceries[index]);
+ }
+ System.out.println("end of getGroceries");
+ }
+
+ 
+   }
+
+
+
+	
+
+
+
+
+
