c0333c640bf2b2036b8b4edafc8ead53a2c208a5
Your Name <you@example.com>
2022-10-13 18:03:39 +0530

    committing classobject files

---
+++ Festival.java
@@ -0,0 +1,19 @@
+class Festival{
+
+Festival(){
+System.out.println("Habba");
+
+}
+String name;
+String food;
+int noOfDays;
+
+void pooje(){
+System.out.println("Pooje");
+
+}
+
+
+
+
+}
---
+++ FestivalTester.java
@@ -0,0 +1,16 @@
+class FestivalTester{
+
+public static void main(String a[]){
+
+Festival fest = new Festival();
+fest.name = "Dipavali";
+fest.food = "Holige";
+fest.noOfDays = 3;
+
+System.out.println(fest.name+ " "+fest.food+" "+fest.noOfDays);
+
+
+
+}
+
+}
