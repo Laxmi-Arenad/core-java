43510e3aae88864082095325ae4af6919c8afae9
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-12 09:39:31 +0530

    Add files via upload

---
+++ Hardware.java
@@ -0,0 +1,34 @@
+package com.xworkz.kal.bean;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.beans.factory.annotation.Qualifier;
+	import org.springframework.stereotype.Component;
+
+	@Component
+	public class Hardware {
+		@Autowired
+		@Qualifier("name")
+		private String name;
+		@Autowired
+		@Qualifier("id")
+		private int id;
+		@Autowired
+		private int gstNo;
+		@Autowired
+		@Qualifier("ownerName")
+		private String ownerName;
+		@Autowired
+		private String address;
+		
+		
+		@Override
+		public String toString() {
+			return "Hardware [name=" + name + ", id=" + id + ", gstNo=" + gstNo + ", ownerName=" + ownerName + ", address="
+					+ address + "]";
+		}
+		
+		
+
+	}
+
+
