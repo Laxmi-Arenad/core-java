990f17468576fe6912e7bfc9ab61d3b2acd2ada6
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-12 09:44:20 +0530

    Add files via upload

---
+++ HardwareConfiguration.java
@@ -0,0 +1,38 @@
+package com.xworkz.kal.configuration;
+
+
+	import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xworkz.kal")
+	public class HardwareConfiguration {
+		
+		public HardwareConfiguration() {
+			
+		}
+		
+		@Bean
+		public String name() {
+			return "Electronics";
+			
+		}
+		@Bean
+		public Integer id() {
+			return 10;
+		}
+		@Bean 
+		public Integer gstNo() {
+			return 312;
+		}
+		@Bean
+		public String ownerName() {
+			return "laxmi";
+		}
+		@Bean
+		public String address() {
+			return "Mudhol";
+		}
+
+	}
