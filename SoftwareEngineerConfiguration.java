db85fc22a77f03b8ccdc9cc2b7d732020cd4e15f
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-12 09:46:19 +0530

    Add files via upload

---
+++ SoftwareEngineerConfiguration.java
@@ -0,0 +1,36 @@
+package com.xworkz.kal.configuration;
+
+
+	
+
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.ComponentScan;
+	import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.xworkz.kal")	public class SoftwareEngineerConfiguration {
+		public SoftwareEngineerConfiguration() {
+			
+		}
+		
+		@Bean
+		public String name(){
+		return "Akshata";
+		
+		}
+		@Bean
+		public Integer salary() {
+			return 40000;
+		}
+		@Bean
+		public String companyName() {
+			return "Wiproo";
+		}
+		@Bean 
+		public Integer experience() {
+			return 2;
+		}
+	}
+
+
