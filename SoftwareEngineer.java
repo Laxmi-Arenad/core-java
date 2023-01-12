032bfcdc6a07c4a1cdc2ef6288f2842dfcc15064
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-12 09:41:28 +0530

    Add files via upload

---
+++ SoftwareEngineer.java
@@ -0,0 +1,34 @@
+package com.xworkz.kal.bean;
+
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.beans.factory.annotation.Qualifier;
+	import org.springframework.stereotype.Component;
+
+	@Component
+
+	public class SoftwareEngineer {
+		@Autowired
+		@Qualifier("name")
+		private String name;
+		@Autowired
+		@Qualifier("salary")
+		private int salary;
+		@Autowired
+		private String companyName;
+		@Autowired
+		private int experience;
+		
+		
+		@Override
+		public String toString() {
+			return "SoftwareEngineer [name=" + name + ", salary=" + salary + ", companyName=" + companyName
+					+ ", experience=" + experience + "]";
+		}
+		
+		
+
+	}
+
+
+
