41221907a17d590381a7147ef75f37222cee3c78
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-17 19:52:09 +0530

    Add files via upload

---
+++ DmartTester.java
@@ -0,0 +1,36 @@
+class DmartTester{ 
+
+  public static void main (String shopping[]){
+	  
+	  
+      String vegatables[] = {"carrat", "Lady's Finger", "Tomato", "Potato", "Onion"};
+	  String perfumes[] = {"Fogg", "Yardley", "Eva", "Molecule", "Romance Eau de"};
+      String biscuites[] = {"Monaco", "Oreo", "Happy Happy", "Marie", "Good Day"};
+      String chocolates[] = {"DiaryMilk", "Perk", "Kitkat", "Crunch", "MilyBar"};
+      String meat[] = {"Beef", "Chicken", "Fish", "Pork", "Turkey"};  
+      String electronicDevices[] = {"Television", "Printer", "Oven", "Projector", "Speaker"};
+      String clothes[] = {"Shirt", "Jeans", "Coat", "Scarf", "Tie"};
+      String shoes[] = {"Jelly", "Chesea Boots", "Desert Boots", "Brougues", "Oxford"};
+      String groceries[] = {"Sugar", "Jaggery", "Rava", "Salt", "Maida"};
+
+  
+      Dmart.getVegatables(vegatables);
+	  Dmart.getPerfumes(perfumes);
+	  Dmart.getBiscuites(biscuites);
+	  Dmart.getChocolates(chocolates); 
+	  Dmart.getMeat(meat);
+      Dmart.getElectronicDevices(electronicDevices);
+      Dmart.getClothes(clothes);
+      Dmart.getShoes(shoes);
+      Dmart.getGroceries(groceries);
+
+ }
+
+
+
+
+
+
+
+
+}
