3856d3e02048a88fa0c377fe499d5300f77c396d
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-24 10:01:54 +0530

    Add files via upload

---
+++ VisaServlet.java
@@ -0,0 +1,26 @@
+package com.xworkz.tetra.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 7, urlPatterns = "/passport")
+public class VisaServlet extends HttpServlet {
+	
+
+	public VisaServlet() {
+		System.out.println("Created. " + this.getClass().getSimpleName());
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Runnning VisaServlet in srver");
+	}
+}
+	
+
+
