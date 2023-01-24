01c99849243744a483864668c67b5d1fb14892db
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-24 09:59:24 +0530

    Add files via upload

---
+++ FastrackServlet.java
@@ -0,0 +1,23 @@
+package com.xworkz.tetra.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 2, urlPatterns = "/call")
+public class FastrackServlet extends HttpServlet {
+
+	public FastrackServlet() {
+		System.out.println("Created. " + this.getClass().getSimpleName());
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Runnning FastrackServlet in srver");
+	}
+}
+
