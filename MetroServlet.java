f5b7fe0252ef1a89b1938e3e538ab6d4016349fb
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-01-24 10:00:15 +0530

    Add files via upload

---
+++ MetroServlet.java
@@ -0,0 +1,25 @@
+package com.xworkz.tetra.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(loadOnStartup = 4, urlPatterns = "/trainName")
+public class MetroServlet extends HttpServlet {
+
+
+
+	public MetroServlet() {
+		System.out.println("Created. " + this.getClass().getSimpleName());
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Runnning MetroServlet in server");
+	}
+}
