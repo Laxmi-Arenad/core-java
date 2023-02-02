19de57be129eaa0e871e18546023fa9cf3c9d84f
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-02-02 08:46:07 +0530

    Add files via upload

---
+++ FiftyRupeeComponent.java
@@ -0,0 +1,12 @@
+package com.xworkz.laserr.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FiftyRupeeComponent {
+
+	public FiftyRupeeComponent() {
+	System.out.println("created"+this.getClass().getSimpleName());
+		
+	}
+}
