7c9161886d9545cd1a0739950998d82ec315704e
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-02-06 09:33:49 +0530

    Add files via upload

---
+++ BeachDTO.java
@@ -0,0 +1,12 @@
+package com.xworkz.beaches.DTO;
+
+import lombok.Data;
+
+@Data
+public class BeachDTO {
+
+	private String name;
+	private String location;
+	private String clean;
+	private String game;
+}
---
+++ CasinoDTO.java
@@ -0,0 +1,13 @@
+package com.xworkz.beaches.DTO;
+
+import lombok.Data;
+
+@Data
+public class CasinoDTO {
+
+	private String name;
+	private String cruise;
+	private String entryFees;
+	private String freeFood;
+	
+}
