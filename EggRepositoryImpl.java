50f2af117cc7352057686e8c93447474b01d5dea
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-02-13 10:17:55 +0530

    Add files via upload

---
+++ EggRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.xworkz.egg.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xworkz.egg.dto.EggDTO;
+
+@Repository
+public class EggRepositoryImpl implements EggRepository{
+
+	@Autowired
+	private EntityManagerFactory entityManageFactory;
+	
+	
+	public EggRepositoryImpl() {
+		System.out.println("created"+this.getClass().getSimpleName());
+		
+	}
+	@Override
+	public boolean save(EggDTO dto) {
+		System.out.println("running save in egg repo..");
+		EntityManager entityManager = this.entityManageFactory.createEntityManager();
+		EntityTransaction transaction=entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(dto);
+		transaction.commit();
+		entityManager.close();
+		return false;
+	}
+}
