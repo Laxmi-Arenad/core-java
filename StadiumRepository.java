1cc0062ac1a5b40bcc31b2319d20deefa1a35408
Laxmi-Arenad <113691550+Laxmi-Arenad@users.noreply.github.com>
2023-03-01 16:02:35 +0530

    Add files via upload

---
+++ StadiumRepository.java
@@ -0,0 +1,22 @@
+package com.xworkz.stadium.repository;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.xworkz.stadium.entity.StadiumEntity;
+import com.xworkz.stadium.entity.StadiumEntity;
+
+public interface StadiumRepository {
+	
+	boolean save(StadiumEntity entity);
+
+	default StadiumEntity findById(int id) {
+		return null;
+	}
+	
+	default List<StadiumEntity> findByGames(String games) {
+		return Collections.emptyList();	
+	}
+   }
+
+
---
+++ StadiumRepositoryImpl.java
@@ -0,0 +1,72 @@
+package com.xworkz.stadium.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xworkz.stadium.entity.StadiumEntity;
+
+@Repository
+public class StadiumRepositoryImpl implements StadiumRepository {
+	
+	
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+	
+	public  StadiumRepositoryImpl() {
+	System.out.println("created " + this.getClass().getSimpleName());
+	}
+	
+	@Override
+	public boolean save(StadiumEntity entity) {
+	System.out.println("running save in repository..."); 
+	
+	EntityManager manager=this.entityManagerFactory.createEntityManager();
+	EntityTransaction transaction = manager.getTransaction();
+	transaction.begin();
+	manager.persist(entity);
+	transaction.commit();
+	manager.close();
+	return true;
+		
+	}
+	
+    @Override
+	public StadiumEntity findById(int id) {
+	System.out.println("Find by id in repo " + id);
+	
+	EntityManager manager=this.entityManagerFactory.createEntityManager();
+	StadiumEntity stadiumEntity = manager.find(StadiumEntity.class, id);
+	manager.close();
+	
+	return stadiumEntity;
+	}
+    
+@Override
+	public List<StadiumEntity> findByGames(String games) {
+	
+	EntityManager manager=this.entityManagerFactory.createEntityManager();
+	//DECIDE :TX,Query?
+	try {
+		Query query =manager.createNamedQuery("findByGames");
+		query.setParameter("gam", games);
+		List<StadiumEntity>list=query.getResultList();
+		System.out.println("Total List found in repo"+list.size());
+	
+	return list;
+	
+	}finally {
+		manager.close();
+		
+}
+	
+}
+}
+
+
